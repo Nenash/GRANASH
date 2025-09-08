20b4abdd3a0331827a2c51e8dd8238ddc9f2be02
ARTEM NENASHEV <artem.nenashev@gmail.com>
2025-09-08 12:07:31 +0300

    Update GranularEngine.h

--- Source/GranularEngine.h
+++ Source/GranularEngine.h
@@ -1,22 +1,37 @@
 #pragma once
 #include <JuceHeader.h>
 
-struct GrainParameters
-{
-    float grainSizeMs {100.0f};
-    float density {10.0f};
-    float position {0.5f};
-    float pitch {0.0f};
-};
-
 class GranularEngine
 {
 public:
-    void prepare(double sampleRate, int samplesPerBlock);
-    void loadSample(const juce::File& file);
+    GranularEngine() {}
+
+    void prepare (double newSampleRate, int /*samplesPerBlock*/)
+    {
+        sampleRate = newSampleRate;
+    }
+
+    // Load a file into internal buffer
+    bool loadFile (const juce::File& file)
+    {
+        juce::AudioFormatManager formatManager;
+        formatManager.registerBasicFormats();
+
+        std::unique_ptr<juce::AudioFormatReader> reader (formatManager.createReaderFor(file));
+
+        if (reader.get() == nullptr)
+            return false;
+
+        sampleBuffer.setSize ((int) reader->numChannels, (int) reader->lengthInSamples);
+        reader->read (&sampleBuffer, 0, (int) reader->lengthInSamples, 0, true, true);
+
+        return true;
+    }
+
     juce::AudioBuffer<float>* getSampleBuffer() { return &sampleBuffer; }
-    GrainParameters params;
+    double getSampleRate() const { return sampleRate; }
 
 private:
+    double sampleRate { 44100.0 };
     juce::AudioBuffer<float> sampleBuffer;
 };
